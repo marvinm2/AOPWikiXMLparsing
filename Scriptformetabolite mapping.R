d2260163d2f9abd186cb8dc59aedf10e40e4db53
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2018-10-09 01:34:25 +0200

    Add files via upload

---
+++ Scriptformetabolite mapping.R
@@ -0,0 +1,19 @@
+install.packages("rJava")
+source("https://bioconductor.org/biocLite.R")
+biocLite("BridgeDbR")
+
+library("BridgeDbR", lib.loc="~/R/win-library/3.4")
+library("RCurl", lib.loc="~/R/win-library/3.4")
+
+#download .bridge file from Figshare
+mbmaps = loadDatabase("C:/Users/marvin.martens/Documents/R/metabolites_20180201.bridge")
+setwd("C:/Users/marvin.martens/Documents/R")
+CASRNs <- read.table("~/R/ListofCasrns.txt", quote="\"", comment.char="")
+
+mapperFunction = function(x) {
+  mappings = paste(map(mbmaps, "Ca", x, "Ce"),collapse = ",")
+  return(mappings)
+}
+
+CHEBI = cbind(sapply(as.character(CASRNs[,1]), mapperFunction))
+write.table(CHEBI, "Mapping_results.txt", sep="\t",col.names = FALSE, quote = FALSE)
